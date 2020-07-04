e3d6fa0c94dc094c6e7a44f73c9d70c6c8229578
Jeoronei Ribinski <jeoronei.ribinski@contabilizei.com.br>
2020-07-04 18:17:31 -0300

    Inclusão subtests e valor default a nome está vazio

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Ney")
-	esperado := "Olá, Ney"
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
 
-	if resultado != esperado {
-		t.Errorf("resultado: %s, esperado: %s", resultado, esperado)
+		if resultado != esperado {
+			t.Errorf("resultado: %s, esperado: %s", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Ney")
+		esperado := "Olá, Ney"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz 'Olá, Mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
 }
